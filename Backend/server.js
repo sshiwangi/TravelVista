cf49df71f63c6e25f45974475dc0298dcc5c6b3b
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-11-04 12:52:40 +0530

    added backend

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes')
+const HttpError = require('./models/http-error');
+const app = express();
+
+app.use(bodyParser.json())//parsing the body
+
+app.use('/api/places', placesRoutes); //=> /api/places/..
+app.use('/api/users', usersRoutes)
+
+app.use((req, res, next) => {
+   const error = new HttpError('Could not find this route.', 404)
+   throw error;
+})
+
+app.use((error, req, res, next) => {//error handling middleware function
+   if(res.headerSent) {
+    return next(error);
+   }
+   res.status(error.code || 500);
+   res.json({message: error.message || 'An unknown error occured'});
+})
+app.listen(5000);
---
+++ Backend/util/location.js
@@ -0,0 +1,9 @@
+const API_KEY = '';
+
+//dummy function
+function getCoordsForAddress(address) {
+    return {
+        lat: 40.7484474,
+        lng: -73.9871516
+    }
+}
