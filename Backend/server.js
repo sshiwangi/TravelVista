4e6c310b567b79fd29c85410d34561c37e88b5b8
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-11-09 23:15:18 +0530

    added env port

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,7 @@ mongoose
   )
   .then(() => {
     console.log("database connected successfully");
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
